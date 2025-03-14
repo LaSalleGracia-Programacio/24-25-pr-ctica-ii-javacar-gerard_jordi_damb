a9c9e659a8a1933b45f28799288f232fbc55f8e4
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-03-14 13:27:23 +0100

    Creacion clase gestor lloger

---
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -0,0 +1,16 @@
+package org.JavaCar;
+
+import java.util.ArrayList;
+
+public class GestorLloguers {
+    public static double calcularIngressosTotals(ArrayList<Vehicle> l, int dies) {
+        double total;
+
+        return total;
+    }
+    public static ArrayList<Vehicle> filtrarPerPreu(ArrayList<Vehicle> l, double preuMax) {
+        ArrayList<Vehicle> r;
+        
+        return r;
+    }
+}
