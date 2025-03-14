a0e4e1cb8f12c259d57ad1948dbfcc3804662a03
Gerard <gerardforness@gmail.com>
2025-03-14 12:35:19 +0100

    getters vehicle

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -18,6 +18,33 @@ public abstract class Vehicle {
         this.preuBase = preuBase;
         this.motor = motor;
         this.rodes = rodes;
+    }
 
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPreuBase() {
+        return preuBase;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public ArrayList<Roda> getRodes() {
+        return rodes;
+    }
+
+    public EtiquetaAmbiental getEtiquetaAmbiental() {
+        return etiquetaAmbiental;
     }
 }
