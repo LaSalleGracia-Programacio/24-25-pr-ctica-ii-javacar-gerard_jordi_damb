d65d9fa70d5fef2813cf4810fc2174bbc7c744ba
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-03-14 12:10:11 +0100

    Creacion clase Rueda

---
+++ src/main/java/org/JavaCar/Rueda.java
@@ -0,0 +1,11 @@
+package org.JavaCar;
+
+public class Rueda {
+    private String marca;
+    private double diametro;
+
+    public Rueda(String marca, double diametro) {
+        this.marca = marca;
+        this.diametro = diametro;
+    }
+}
