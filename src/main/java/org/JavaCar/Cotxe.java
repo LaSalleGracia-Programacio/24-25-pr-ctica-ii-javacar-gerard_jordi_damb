15799f6dc5a33a813db50c7ea73ceab956362ce4
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-03-14 12:33:28 +0100

    correccion coche y creacion clase moto

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -3,10 +3,10 @@ package org.JavaCar;
 import java.util.ArrayList;
 
 public class Cotxe extends Vehicle {
-    private double capacitatCarga;
+    private int nombrePlaces;
 
-    public Cotxe(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes, double capacitatCarga) {
+    public Cotxe(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes, int nombrePlaces) {
         super(matricula, marca, model, preuBase, motor, rodes);
-        this.capacitatCarga = capacitatCarga;
+        this.nombrePlaces = nombrePlaces;
     }
 }
--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -1,4 +1,12 @@
 package org.JavaCar;
 
-public class Moto {
+import java.util.ArrayList;
+
+public class Moto extends Vehicle {
+    private int cilindrada;
+
+    public Moto(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes, int cilindrada) {
+        super(matricula, marca, model, preuBase, motor, rodes);
+        this.cilindrada = cilindrada;
+    }
 }
