1e3adfa616c581417aff8a24ca0cfd32d9ae4913
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-03-14 12:35:15 +0100

    creacion clase furgoneta

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -1,4 +1,12 @@
 package org.JavaCar;
 
-public class Furgoneta {
+import java.util.ArrayList;
+
+public class Furgoneta extends Vehicle {
+    private double capacitatCarga;
+
+    public Furgoneta(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes, double capacitatCarga) {
+        super(matricula, marca, model, preuBase, motor, rodes);
+        this.capacitatCarga = capacitatCarga;
+    }
 }
