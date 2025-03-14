140740e6d5cf1c20e1f3b55f5381733931dbfa21
Gerard <gerardforness@gmail.com>
2025-03-14 12:18:05 +0100

    constructor vehicle abstracte

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -2,7 +2,7 @@ package org.JavaCar;
 
 import java.util.ArrayList;
 
-public class Vehicle {
+public abstract class Vehicle {
     private String matricula;
     private String marca;
     private String model;
@@ -11,5 +11,13 @@ public class Vehicle {
     private ArrayList<Roda> rodes;
     private EtiquetaAmbiental etiquetaAmbiental;
 
-    
+    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+
+    }
 }
