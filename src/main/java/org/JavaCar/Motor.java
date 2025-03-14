e3aa253dba498a1b0a5c104874c4ea91b7036b03
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-03-14 12:07:47 +0100

    Creacion clase motor

---
+++ src/main/java/org/JavaCar/Motor.java
@@ -0,0 +1,11 @@
+package org.JavaCar;
+
+public class Motor {
+    private String tipo;
+    private int potencia;
+
+    public Motor(String tipo, int potencia) {
+        this.tipo = tipo;
+        this.potencia = potencia;
+    }
+}
