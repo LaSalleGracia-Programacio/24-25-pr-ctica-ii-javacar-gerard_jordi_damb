72f00576e8faa6336cc591735670f2da3b223315
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-03-14 13:17:53 +0100

    Cambio de orden en constructores

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -1,11 +1,9 @@
 package org.JavaCar;
 
-import java.util.ArrayList;
-
 public class Cotxe extends Vehicle {
     private int nombrePlaces;
 
-    public Cotxe(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, int nombrePlaces) {
+    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor motor, Roda[] rodes) {
         super(matricula, marca, model, preuBase, motor, rodes);
         this.nombrePlaces = nombrePlaces;
     }
--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -1,11 +1,9 @@
 package org.JavaCar;
 
-import java.util.ArrayList;
-
 public class Furgoneta extends Vehicle {
     private double capacitatCarga;
 
-    public Furgoneta(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, double capacitatCarga) {
+    public Furgoneta(String matricula, String marca, String model, double preuBase, double capacitatCarga, Motor motor, Roda[] rodes) {
         super(matricula, marca, model, preuBase, motor, rodes);
         this.capacitatCarga = capacitatCarga;
     }
