eba6df6ebe9180c57613d762ba47445fcff95287
Gerard <gerardforness@gmail.com>
2025-03-14 12:14:18 +0100

    atributs vehicle

---
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -0,0 +1,15 @@
+package org.JavaCar;
+
+import java.util.ArrayList;
+
+public class Vehicle {
+    private String matricula;
+    private String marca;
+    private String model;
+    private double preuBase;
+    private Motor motor;
+    private ArrayList<Roda> rodes;
+    private EtiquetaAmbiental etiquetaAmbiental;
+
+    
+}
