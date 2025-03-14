1101744156412d186b4d9cb1659867f53f3a995c
Gerard <gerardforness@gmail.com>
2025-03-14 13:56:11 +0100

    calcular preu total acabt

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorLloguers {
-    public static double calcularIngressosTotals(List<Vehicle> v, int dies) {
+    public static double calcularIngressosTotals(List<Vehicle> vehicles, int dies) {
         double total = 0;
+        for (int i = 0; i < vehicles.size(); i++) {
+            total += vehicles.get(i).calcularPreu(dies);
+        }
         return total;
     }
     public static List<Vehicle> filtrarPerPreu(List<Vehicle> v, double preuMax) {
