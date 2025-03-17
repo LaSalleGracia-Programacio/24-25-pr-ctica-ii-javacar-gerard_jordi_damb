b5084448529ac144bcaaea0ea1351a9eeee2a81f
Gerard <gerardforness@gmail.com>
2025-03-17 11:58:06 +0100

    calcularEtiqueta moto

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -30,6 +30,26 @@ public class Moto extends Vehicle {
         return cilindrada;
     }
 
+    public EtiquetaAmbiental calcularEtiqueta() {
+        if ((this.tipo == Tipo.PHEV || this.tipo == Tipo.HEV) && this.autonomia < 40) {
+            return EtiquetaAmbiental.Eco;
+        }
+        else if (this.tipo == Tipo.BEV || this.tipo == Tipo.REEV
+                || (this.tipo == Tipo.PHEV && autonomia > 40) || this.tipo == Tipo.Pila) {
+            return EtiquetaAmbiental.ZeroEmissions;
+        }
+        else if (this.euro == 2) {
+            return EtiquetaAmbiental.B;
+        }
+        else if (this.euro == 3 || this.euro == 4) {
+            return EtiquetaAmbiental.C;
+        }
+        else {
+            return EtiquetaAmbiental.SenseEtiqueta;
+        }
+    }
+
+
     @Override
     public double calcularPreu(int dies) {
         if (this.getCilindrada() > 500) {
