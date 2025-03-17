a9110c1e6584719b9f6b13bb178bbbe70849854f
Gerard <gerardforness@gmail.com>
2025-03-17 11:50:23 +0100

    calcularEtiqueta cotxe

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -29,4 +29,28 @@ public class Cotxe extends Vehicle {
     public int getNombrePlaces() {
         return nombrePlaces;
     }
+
+    public EtiquetaAmbiental calcularEtiqueta() {
+        if (((this.tipo == Tipo.PHEV && autonomia < 40) || this.tipo == Tipo.HEV
+                || this.tipo == Tipo.GNL || this.tipo == Tipo.GNC || this.tipo == Tipo.GLP)
+                && (this.combustible == Combustible.Gasolina && (this.euro == 4 || this.euro == 5 || this.euro == 6)
+                || (this.combustible == Combustible.Gasoil && this.euro == 6))) {
+            return EtiquetaAmbiental.Eco;
+        }
+        else if (this.tipo == Tipo.BEV || this.tipo == Tipo.REEV
+                || (this.tipo == Tipo.PHEV && autonomia < 40) || this.tipo == Tipo.Pila) {
+            return EtiquetaAmbiental.ZeroEmissions;
+        }
+        else if ((this.combustible == Combustible.Gasolina && this.euro == 3) ||
+                (this.combustible == Combustible.Gasoil && (this.euro == 4 || this.euro == 5)) ) {
+            return EtiquetaAmbiental.B;
+        }
+        else if ((this.combustible == Combustible.Gasolina && (this.euro == 4 || this.euro == 5 || this.euro == 6))
+                || (this.combustible == Combustible.Gasoil && this.euro == 6)) {
+            return EtiquetaAmbiental.C;
+        }
+        else {
+            return EtiquetaAmbiental.SenseEtiqueta;
+        }
+    }
 }
